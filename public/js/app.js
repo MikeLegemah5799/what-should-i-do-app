422ba2b7590b043b0ee5375002e439216ec28083
Michael Legemah <MichaelLegemah@gmail.com>
2018-03-20 20:51:15 -0400

    compiled jsx using babel

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,13 @@
 console.log('App is running!');
 
 // JSX - JavaScript XML
-var template = <p>This is the JSX running from app.js!</p>;
+//var template = <p>This is the JSX running from app.js!</p>;
 var appRoot = document.getElementById('app');
+var template = React.createElement(
+    "h1",
+    { id: "someid" },
+    "This is the JSX running from app.js!"
+  );
+
 
 ReactDOM.render(template, appRoot);
