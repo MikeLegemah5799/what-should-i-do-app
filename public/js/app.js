6b107348d67772db33913625ea89d8ea66a99d86
Michael Legemah <MichaelLegemah@gmail.com>
2018-03-20 21:14:34 -0400

    set up babel commands

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,13 @@
+'use strict';
+
 console.log('App is running!');
 
 // JSX - JavaScript XML
-//var template = <p>This is the JSX running from app.js!</p>;
-var appRoot = document.getElementById('app');
 var template = React.createElement(
-    "h1",
-    { id: "someid" },
-    "This is the JSX running from app.js!"
-  );
-
+  'p',
+  null,
+  'Does This Change!'
+);
+var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App is running!');
+
+// JSX - JavaScript XML
+var template = <p>Does This Change!</p>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
