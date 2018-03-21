119f99192f790ecd6d51f0d38699bcfa4912f85a
Michael Legemah <MichaelLegemah@gmail.com>
2018-03-20 23:20:02 -0400

    adjusrted js

--- public/js/app.js
+++ public/js/app.js
@@ -4,10 +4,47 @@ console.log('App is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'p',
-  null,
-  'Does This Change!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'What Should I do App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    )
 );
+
+var user = {
+    name: 'Mike',
+    age: 39,
+    location: 'New York'
+};
+var bio = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        use.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(bio, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 console.log('App is running!');
 
 // JSX - JavaScript XML
-var template = <p>Does This Change!</p>;
+var template = (
+    <div>
+        <h1>What Should I do App</h1> 
+        <p>This is some info</p>
+    </div>
+);
+
+var user = {
+    name: 'Mike',
+    age: 39,
+    location: 'New York'
+};
+var bio = (
+    <div>
+        <h1>{use.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(bio, appRoot);
