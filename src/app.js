2c9379a3f9d1668febe877e1ed40e13891591007
Michael Legemah <MichaelLegemah@gmail.com>
2018-04-02 11:49:58 -0400

    adding react components

--- src/app.js
+++ src/app.js
@@ -1,26 +1,61 @@
-console.log('App is running!');
+class IndecisionApp extends React.Component {
+    render() {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer';
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle} />
+                <Action />
+                <Options />
+                <AddOptions />
+            </div>
+        );
+    }
+}
 
-// JSX - JavaScript XML
-var template = (
-    <div>
-        <h1>What Should I do App</h1> 
-        <p>This is some info</p>
-    </div>
-);
+class Header extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
+            </div>
+        );
+    }
+}
 
-var user = {
-    name: 'Mike',
-    age: 39,
-    location: 'New York'
-};
-var bio = (
-    <div>
-        <h1>{use.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
-    </div>
-);
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }   
+}
 
-var appRoot = document.getElementById('app');
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                Options here 
+            </div>
+        );
+    }
 
-ReactDOM.render(bio, appRoot);
+}
+
+class AddOptions extends React.Component {
+    render() {
+        return (
+            <div>
+                AddOptions here 
+            </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
